73f94370c888adf18b6fd3ef8a7b0e5b7f7ff229
sourav19071999 <souravrout869@gmail.com>
2026-06-30 18:40:09 +0530

    Add Room type converters

---
+++ app/src/main/java/com/vyapar/securecallarchive/data/local/database/RoomTypeConverters.kt
@@ -0,0 +1,19 @@
+package com.vyapar.securecallarchive.data.local.database
+
+import androidx.room.TypeConverter
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class RoomTypeConverters {
+    private val formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+
+    @TypeConverter
+    fun fromLocalDateTime(value: LocalDateTime?): String? {
+        return value?.format(formatter)
+    }
+
+    @TypeConverter
+    fun toLocalDateTime(value: String?): LocalDateTime? {
+        return value?.let { LocalDateTime.parse(it, formatter) }
+    }
+}
