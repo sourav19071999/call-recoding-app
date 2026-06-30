68d7590ea55b7557fdbeb88a2f35c8243b9f501e
sourav19071999 <souravrout869@gmail.com>
2026-06-30 18:35:51 +0530

    Add main activity

---
+++ app/src/main/java/com/vyapar/securecallarchive/ui/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.vyapar.securecallarchive.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.vyapar.securecallarchive.ui.theme.SecureCallArchiveTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SecureCallArchiveTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppNavigation()
+                }
+            }
+        }
+    }
+}
